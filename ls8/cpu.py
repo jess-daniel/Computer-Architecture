336f7dc1b309f83f19495fce5c1b24c4d54113f6
Jess Daniel <jessdaniel474@gmail.com>
2020-09-15 13:11:33 -0400

    ls8 prints 8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,21 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +27,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        PRN = 0b01000111
+        HLT = 0b00000001
+        LDI = 0b10000010
+
+        running = True
+
+        while running:
+            command = self.ram[self.pc]
+
+            if command == PRN:
+                num = self.ram[self.pc + 1]
+                print(self.reg[num])
+                self.pc += 2
+
+            elif command == LDI:
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif command == HLT:
+                running = False
+                self.pc += 1
+
+            else:
+                print(f'{command} not found')
+                sys.exit(1)
