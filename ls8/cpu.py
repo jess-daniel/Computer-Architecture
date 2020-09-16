49d63ac522bf06997882b9e9f81fa63f2b6dc792
Jess Daniel <jessdaniel474@gmail.com>
2020-09-16 16:45:46 -0400

    mult function is working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -18,26 +18,23 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    comment_split = line.split("#")
+                    n = comment_split[0].strip()
+                    if n == " ":
+                        continue
+                    if n:
+                        value = int(n, 2)
+                        self.ram[address] = value
+                        address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {filename} not found")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -45,6 +42,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -70,24 +69,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        PRN = 0b01000111
         HLT = 0b00000001
         LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
 
         running = True
 
         while running:
             command = self.ram[self.pc]
 
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
             if command == PRN:
-                num = self.ram[self.pc + 1]
-                print(self.reg[num])
+                print(self.reg[operand_a])
                 self.pc += 2
 
             elif command == LDI:
-                operand_a = self.ram_read(self.pc + 1)
-                operand_b = self.ram_read(self.pc + 2)
                 self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif command == MUL:
+                self.alu("MUL", operand_a, operand_b)
                 self.pc += 3
 
             elif command == HLT:
